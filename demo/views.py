7777a4f87fc2de51bc8aa208c601539ff474bb86
sunyi00 <sunyi00@gmail.com>
2015-04-20 09:28:26 +0800

    m2: index with flags

--- demo/views.py
+++ demo/views.py
@@ -6,12 +6,22 @@ from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 
+from flags.models import Flag
+from foods.models import Food
+
 
 def index(request):
-    return HttpResponse('demo index')
+    color_flag = Flag.objects.get(name='color')
+    if color_flag:
+        return HttpResponse(color_flag.status)
+    else:
+        return HttpResponse('white')
 
 def foods(request):
-    return JsonResponse('')
+    food_list = []
+    for food in Food.objects.all():
+        food_list.append({food.name: food.id})
+    return JsonResponse({'foods': food_list})
 
 def food_detail(request, foodid):
     return JsonResponse('')
